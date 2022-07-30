9ba485402abb673a19aa5629dbcd6da0ef01f994
Taro Matsuzawa <btm@tech.email.ne.jp>
2022-07-30 11:47:47 +0900

    add maplibre-gl

---
+++ src/js/map.ts
@@ -0,0 +1,10 @@
+import 'maplibre-gl/dist/maplibre-gl.css'
+
+import { Map } from 'maplibre-gl'
+
+const map = new Map({
+  container: 'map',
+  style: 'https://tile.openstreetmap.jp/styles/maptiler-toner-ja/style.json',
+  center: [139.7531, 35.68302],
+  zoom: 9
+})
