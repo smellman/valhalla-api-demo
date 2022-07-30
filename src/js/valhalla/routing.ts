1c8da9b6caf6400306a372c4e100343c36d436b4
Taro Matsuzawa <btm@tech.email.ne.jp>
2022-07-30 14:51:36 +0900

    WIP: development valhalla API

--- src/js/valhalla/routing.ts
+++ src/js/valhalla/routing.ts
@@ -1,28 +1,53 @@
 import { Valhalla } from './valhalla'
 import { LngLat } from 'maplibre-gl'
 
-export type Costing =
-  | 'auto'
-  | 'bicycle'
-  | 'bus'
-  | 'bikeshare'
-  | 'truck'
-  | 'taxi'
-  | 'moter_scooter'
-  | 'motorcycle'
-  | 'multimodal'
-  | 'pedestrian'
-export type Units = 'kilometers' | 'miles'
-export type Language =
-  | 'bg-BG'
-  | 'ca-ES'
-  | 'cs-CZ'
-  | 'da-DK'
-  | 'de-DE'
-  | 'el-GR'
-  | 'en-GB'
-  | 'en-US-x-pirate'
-  | ''
+export const CostingArray = [
+  'auto',
+  'bicycle',
+  'bus',
+  'bikeshare',
+  'truck',
+  'taxi',
+  'moter_scooter',
+  'motorcycle',
+  'multimodal',
+  'pedestrian',
+] as const
+export type Costing = typeof CostingArray[number]
+export const UnitsArray = ['kilometers', 'miles']
+export type Units = typeof UnitsArray[number]
+export const LanguageArray = [
+  'bg-BG',
+  'ca-ES',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'el-GR',
+  'en-GB',
+  'en-US-x-pirate',
+  'en-US',
+  'es-ES',
+  'et-EE',
+  'fi-FI',
+  'fr-FR',
+  'hi-IN',
+  'hu-HU',
+  'it-IT',
+  'ja-JP',
+  'nb-NO',
+  'nl-NL',
+  'pl-PL',
+  'pt-BR',
+  'pt-PT',
+  'ro-RO',
+  'ru-RU',
+  'sk-SK',
+  'sl-SI',
+  'sv-SE',
+  'tr-TR',
+  'uk-UA',
+] as const
+export type Language = typeof LanguageArray[number]
 
 export class Routing extends Valhalla {
   /**
@@ -31,6 +56,8 @@ export class Routing extends Valhalla {
   public routing(
     start_position: LngLat,
     end_point: LngLat,
-    costing?: Costing
+    costing: Costing = 'auto',
+    language: Language = 'ja-JP',
+    units: Units = 'kilometers'
   ) {}
 }
